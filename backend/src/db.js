2d17faa373c34844f9080b7a5129e42e05878daf
Veeti Mäkinen <Veeti.makinen141@gmail.com>
2026-01-26 20:45:44 +0200

    updated db.js to fit more modern node version

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,21 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+let client;
 
 export async function getCollection() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("MONGODB_URI is undefined. Check your .env file!");
+  }
+  if (!client) {
+    client = new MongoClient(uri);
+  }
+
   if (!client.topology?.isConnected()) {
     await client.connect();
   }
+
   const db = client.db(process.env.DB_NAME);
   return db.collection(process.env.COLLECTION_NAME);
 }
